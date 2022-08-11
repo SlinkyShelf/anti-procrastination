245cf1f4e739e16e985a16f60c7ceb3f8eb859ee
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-08-10 23:20:33 -0500

    Started work on inject.js and changed the extension name

---
+++ inject.js
@@ -0,0 +1,12 @@
+const cover = document.createElement("div")
+cover.style.width = "100vw"
+cover.style.height = "100vh"
+cover.style.zIndex = 1000
+cover.style.backgroundColor = "red"
+
+// Position
+
+
+document.body.appendChild(cover)
+
+console.log("Test")
