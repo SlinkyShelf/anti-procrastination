fd11a96870d2fe96a4486e0ddf15078f0ce92b20
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-08-20 23:54:33 -0500

    Actually blocks stuff now

--- inject.js
+++ inject.js
@@ -1,10 +1,14 @@
 const cover = document.createElement("div")
 cover.style.width = "100vw"
-cover.style.height = "100vh"
-cover.style.zIndex = 1000
-cover.style.backgroundColor = "red"
+cover.style.height = "10000vh"
+cover.style.zIndex = 1000000
+cover.style.backgroundColor = "#656566"
 
-// Position
+cover.style.position = "absolute"
+cover.style.top = "0px"
+cover.style.left = "0px"
+
+document.body.style.overflow = "hidden"
 
 
 document.body.appendChild(cover)
